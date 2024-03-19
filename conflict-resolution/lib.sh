e9b7f7d349bb36127532366f425fe4147e2da0a2
Dan Davison <dan.davison@temporal.io>
2024-03-18 23:10:08 -0400

    🧬

--- conflict-resolution/lib.sh
+++ conflict-resolution/lib.sh
@@ -19,6 +19,10 @@ cr-start-workflow() {
 
 cr-terminate-workflow() {
     local addr=$1
+    [ -n "$addr" ] || {
+        echo "cr-terminate-workflow address" >&2
+        return 1
+    }
     temporal workflow -n $NS --address $addr terminate -w $WID
 }
 
@@ -51,43 +55,64 @@ cr-send-update() {
     temporal workflow -n $NS --address $addr update -w $WID --name my-update --input "\"$input\""
 }
 
-cr-failover() {
-    local cluster=$1
-    [ -n "$cluster" ] && [[ $cluster = cluster-a || $cluster = cluster-b ]] || {
-        echo "cr-failover cluster-a/b" >&2
+cr-set-active() {
+    local addr=$1
+    local cluster=$2
+    [ -n "$addr" ] && [ -n "$cluster" ] && [[ $cluster = cluster-a || $cluster = cluster-b ]] || {
+        echo "cr-set-active address cluster-a/b" >&2
         return 1
     }
-    tctl --ns $NS namespace update --active_cluster $cluster
+    tctl --address $addr --ns $NS namespace update --active_cluster $cluster
     cr-describe-namespace
 }
 
 cr-describe-namespace() {
-    temporal operator namespace describe $NS | grep -F ReplicationConfig.ActiveClusterName
+    temporal --address $A operator namespace describe $NS | grep -F ReplicationConfig.ActiveClusterName
+    temporal --address $B operator namespace describe $NS | grep -F ReplicationConfig.ActiveClusterName
+    cr-query-history-replication
+    cr-query-namespace-replication
 }
 
 # https://github.com/dandavison/temporalio-temporal/tree/simulate-conflict-resolution
-cr-enable-replication() {
-    cr-set-replication-max-id -1
+cr-enable-history-replication() {
+    -cr-set-history-replication-max-id -1
 }
 
-cr-disable-replication() {
-    cr-set-replication-max-id 1
+cr-disable-history-replication() {
+    -cr-set-history-replication-max-id 1
 }
 
-cr-set-replication-max-id() {
-    local id=$1
-    [ -n "$id" ] || {
-        echo "cr-set-replication-max-id" >&2
-        return 1
-    }
-    sed -i '/history.ReplicationMaxEventId:/,/value:/ s/value: .*/value: '$id'\n/' $DYNAMIC_CONFIG
-    cr-query-replication-max-id
+cr-enable-namespace-replication() {
+    -cr-set-namespace-replication true
+}
+
+cr-disable-namespace-replication() {
+    -cr-set-namespace-replication false
+}
+
+-cr-set-namespace-replication() {
+    local value=$1
+    sed -i '/worker.enableNamespaceReplication:/,/value:/ s/value: .*/value: '$value'/' $DYNAMIC_CONFIG
+    cr-query-namespace-replication
     echo -n "\nWaiting 5s for dynamic config change..."
     sleep 5
     echo
 }
 
-cr-query-replication-max-id() {
+cr-query-namespace-replication() {
+    sed -n '/worker.enableNamespaceReplication:/,/value:/p' $DYNAMIC_CONFIG
+}
+
+-cr-set-history-replication-max-id() {
+    local id=$1
+    sed -i '/history.ReplicationMaxEventId:/,/value:/ s/value: .*/value: '$id'/' $DYNAMIC_CONFIG
+    cr-query-history-replication
+    echo -n "\nWaiting 5s for dynamic config change..."
+    sleep 5
+    echo
+}
+
+cr-query-history-replication() {
     sed -n '/history.ReplicationMaxEventId:/,/value:/p' $DYNAMIC_CONFIG
 }
 
